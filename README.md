5e2ab519b5010dd2f95a254c90cac605dfb4c675
Edinei <galera.org@gmail.com>
2017-10-26 19:07:54 -0200

    up

---
+++ README.md
@@ -0,0 +1,296 @@
+# Panel
+
+
+
+#Instalação:
+via cdnjs:
+```html
+<!-- Lembre-se de incluir jQuery :) -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+
+<!-- jQuery Window Panel Modal -->
+<script src="./panel.min.js"></script> <!-- seu caminho para o panel.min.js-->
+<link rel="stylesheet" href="./panel.min.css" /> <!-- seu caminho para o panel.min.css-->
+```
+
+# Abrindo a janela
+
+```js
+$('#button').panel();
+```
+### Elemento target
+a janela abre com animação partindo de um target. 
+O elemento target será o elemento que chamar o panel(). 
+Nesse exemplo acima, será o nosso botão com id = button.
+É possível ainda definir um target diferente dentro dos parâmetros do nosso panel().
+* Veja abaixo um exemplo:
+```js
+$('html').panel({
+    target: $("#button")
+});
+```
+perceba que foi necessário mandarmos o elemento como target, 
+e não uma referência ao seu id, ou class
+#Identificação da Janela
+nossa janela precisa ser única, e por isso é gerado um id de identificação da janela.
+o id é gerado de forma aleatória com números que variam de 0 a 10000000.
+De todo modo, se você precisar manipular esse id, é necessário fornecer seu próprio id.
+* Observe o modelo abaixo:
+```js
+$('#button').panel({
+    id: 'id-da-minha-janela-de-post'
+});
+```
+#Header
+Cabeçalho da janela, podemos nomear nossa janela aqui, e atribuir estilo,
+basta colocarmos o atributo 'header'.
+* vejamos abaixo:
+```js
+$('#button').panel({
+    header: {
+        html: 'Minha Janela',
+        css: {
+            background: 'cyan'
+        }
+    }
+});
+```
+o header.html é o nome da sua janela, e serve para identificação do usuário 
+ao utilizar mais que uma janela. Ao minimizar uma janela, somente este título ficará
+a amostra para identificação. O title tem por padrão largura de 140px, 
+tendo espaço para cerca de 18 caracteres. Para alterar este tamanho, modifique no panel.css
+
+#Conteúdo da Janela
+Para controlar o conteúdo da janela, é preciso atacarmos o atributo 'body'. 
+Onde é possível determinarmos seu conteúdo ('html') e seu estilo ('css').
+* Veja o exemplo abaixo
+###Exemplo de aplicação de estilo:
+```js
+$('#button').panel({
+    body: {
+        html: '<div><h2>Corpo da janela</h2></div>', 
+        css: {
+            'padding': '20px',
+            'font-size': '1.3em'
+        }
+    }
+});
+```
+#Estilo da Janela
+Para modificar o estilo da janela, basta utilizar-mos o atributo 'css'.
+* Veja abaixo
+```js
+$('#button').panel({
+    css: {
+        'background': '#EEEEEE', 
+        'color': 'darkcyan',
+        'opacity': 0.8
+    }
+});
+```
+#Atributos da Janela
+Também é possível definir nossos atributos da janela, tanto para uso de classes,
+quanto para outras atividades.
+* Veja um exemplo
+```js
+$('#button').panel({
+    attr: {
+        title: 'Olá, eu sou o title da janela',
+        class: 'minhaClasse outraClasse',
+        'data-alguma-coisa': 'guarde algo útil para controle'
+    }
+});
+```
+#Botões de Cabeçalho da Janela
+por padrão, a janela inicia com 3 botões, o de minimizar, maximizar e fechar.
+É possível determinar quais botões você quer para sua janela. 
+Para isso, iremos atacar o atributo 'control'.
+#### close: 
+* Descrição: Fecha a janela permanentemente, excluíndo todos os dados da janela.
+* Valor Padrão: true,
+* Valores Aceitos: true, false, 1, 0.
+
+#### maximize: 
+* Descrição: Coloca a janela em tamanho máximo do site. Dando total foco a essa janela
+* Valor Padrão: true,
+* Valores Aceitos: false, true, 1, 0.
+
+#### minimize: 
+* Descrição: Minimiza a janela tirando o foco dela, 
+a janela fica no final do site fixado, com tamanho mínimo.
+* Valor Padrão: true,
+* Valores Aceitos: false, true, 1, 0.
+
+### Confira este exemplo abaixo:
+```js
+$("#button").panel({
+    control: {
+        minimize: false,
+        maximize: false,
+        close: true // não precisa informar o close, pois o valor padrão já é true
+    }
+});
+```
+#Gatilho dos Botões
+Para definirmos outras ações extras para serem executadas ao clicar em close, minimize ou maximize.
+Basta atacarmos o atributo 'control' com os valores 'onClose', 'onMinimize', 'onMaximize'.
+
+#### onClose: 
+* Descrição: Executa uma ação ao fechar a janela.
+* Valor Padrão: null,
+* Valores Aceitos: function()
+
+#### onMinimize: 
+* Descrição: Executa uma ação ao minimizar a janela.
+* Valor Padrão: null,
+* Valores Aceitos: function()
+
+#### onMaximize: 
+* Descrição: Executa uma ação ao maximizar a janela.
+* Valor Padrão: null,
+* Valores Aceitos: function()
+
+### Veja um exemplo
+```js
+$("#button").panel({
+    control: {
+        maximize: false, //remove o botão maximize, conforme vimos no exemplo anterior
+        onMinimize: function() {
+          alert('ok'); //dispara um alert quando clicado em minimize
+          return true; //cancela a função padrão de minimizar.
+        },
+        onClose: minhaFuncaoClose() //minha função declarada em outro local sendo chamada ao fechar a janela.
+    }
+});
+```
+percebam, que no exemplo acima, no atributo 'onMinimize', 
+foi retornado true na função atribuída,
+isso irá cancelar a função padrão de minimizar a janela. Também é aplicável ao 'onClose' e ao 'onMaximize'
+
+#Drag e Resize
+Para não permitir o drag (arrasto da janela), basta atacar o atributo 'control.drag',
+e para não permitir o resize da janela, basta atacar o atributo 'control.resize'.
+
+#### drag: 
+* Descrição: Define se a janela poderá ser movida (drag).
+* Valor Padrão: true,
+* Valores Aceitos: true, false, 1, 0.
+
+#### resize: 
+* Descrição: Define se a janela poderá ser reajustada de tamanho (resize).
+* Valor Padrão: true,
+* Valores Aceitos: true, false, 1, 0.
+
+### Veja o exemplo
+```js
+$("#button").panel({
+    control: {
+        drag: false,// não permite drag da janela
+        resize: true // não precisa informar, pois o padrão já é true.
+    }
+});
+```
+#Gatilho Drag e Resize
+assim como os botões close, minimize e maximize, drag e resize também podem disparar funções.
+Porém somente irá disparar, ao finalizar a ação de drag, ou de resize. 
+E não é possível cancelar a ação padrão, como nos botões close, minimize e maximize.
+
+#### onDrag: 
+* Descrição: Executa uma ação ao finalizar a ação de drag.
+* Valor Padrão: null,
+* Valores Aceitos: function()
+
+#### onResize: 
+* Descrição: Executa uma ação ao finalizar a ação de resize.
+* Valor Padrão: null,
+* Valores Aceitos: function()
+
+### Veja a aplicação do drag e resize abaixo
+```js
+$("#button").panel({
+    control: {
+        onDrag: function() {
+            console.log("você moveu a janela");
+        },
+        onResize: minhaFuncaoResize()
+    }
+});
+```
+# Efeitos
+#### blur: 
+* Descrição: Deixa os elementos dentro do body ofuscados, com excessão das janelas.
+* Valor Padrão: true,
+* Valores Aceitos: true, false, 1, 0.
+
+#### speed: 
+* Descrição: controla velocidade das transições entre ações em segundos.
+* Valor Padrão: 0.25,
+* Valores Aceitos: 0.01 ~ 10
+
+#### clickOut: 
+* Descrição: executa ação de fechamento ao clicar fora da janela quando for true. 
+Minimiza janela quando valor for 'minimize'
+* Valor Padrão: false,
+* Valores Aceitos: false, true, 'minimize'
+#
+#### Confira um exemplo de aplicação do controle de efeitos
+```js
+$('#button').panel({
+    effect: {
+        blur: false, //remove efeito de blur do fundo
+        speed: 0.01, //para remover transição.
+        clickOut: 'minimize' // minimaliza janela se clicar fora dela.
+    }
+});
+```
+
+#Exemplo Completo
+```html
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+<script src="./panel.min.js"></script>
+<link rel="stylesheet" href="./panel.min.css" />
+
+<button id="btnWin">Janela</button>
+
+<sctipt>
+    $("#btnWin").panel({
+        header: {
+            html: 'Demo Janela',
+            css: {
+                background: 'tomato'
+            }
+        },
+        body: {
+            html: '<h2>Conteúdo da janela</h2>',
+            css: {
+                background: '#EEEEEE',
+                padding: '15px 30px'
+            }
+        },
+        control: {
+            resize: false,
+            speed: 0.5,
+            clickOut: 'minimize',
+            onClose: function() {
+                $('html').panel({
+                    header: {
+                        html: 'Fechamento'
+                    },
+                    body: {
+                        html: '<b> Obrigado por usar</b>',
+                        css: {
+                            padding: '20px',
+                            'text-align': 'center',
+                            'font-size': '3em'
+                        }
+                    },
+                    control: {
+                        clickOut: true
+                    }
+                });
+            }
+        }
+    });
+</sctipt>
+
+```
---
+++ panel.js
@@ -0,0 +1,442 @@
+const wwt = parseInt($(window).width());
+const wht = parseInt($(window).height());
+
+$(function ($) {
+
+    $.fn.panel = function (options) {
+        var panel = {
+            id: options.id || Math.floor((Math.random() * 10000000)),
+            title: options.title || "",
+            drag: function ($ontab, $drag) {
+                dragResizeAction($ontab, $drag, 'd', control.resize, control.onResize, control.onDrag);
+            },
+            minimize: function ($panel) {
+                if ($panel.attr("data-minimize") === "1") {
+                    minimizePanelOut($panel, effect.speed, effect.blur, control.resize, control.onResize, control.onDrag);
+
+                } else {
+
+                    minimizePanelIn($panel, effect.speed, effect.blur);
+                    setTimeout(function () {
+                        $panel.find(".ontab-header").off("mousedown").on("click", function () {
+                            panel.minimize($panel);
+                        });
+                    }, effect.speed * 1000);
+                }
+            },
+            maximize: function ($panel) {
+                if ($panel.attr("data-maximize") === "1") maximizePanelOut($panel, effect.speed, control.resize, control.onResize, control.onDrag);
+                else maximizePanelIn($panel, effect.speed, control.resize);
+                panelScrool(effect.blur);
+            },
+            close: function ($panel) {
+                closePanel($panel, effect.blur);
+            }
+        };
+
+        var effect = {
+            speed: 0.25,
+            blur: true
+        };
+        $.extend(effect, options.effect);
+
+        var css = {
+            "top": parseInt(wht * (wwt < 900 ? (wwt < 500 ? 0 : 0.025) : 0.05)),
+            "left": parseInt(wwt * (wwt < 900 ? (wwt < 500 ? 0 : 0.05) : 0.1)),
+            "width": parseInt(wwt * (wwt < 900 ? (wwt < 500 ? 1 : 0.9) : 0.8)),
+            "height": parseInt(wht * (wwt < 900 ? (wwt < 500 ? 1 : 0.95) : 0.9)),
+            "z-index": 1,
+            "transition-duration": effect.speed * 1.3 + "s",
+            "border-radius": 0,
+            "min-width": 190,
+            "max-width": wwt,
+            "min-height": 30,
+            "max-height": wht
+        };
+        $.extend(css, options.css);
+
+        var control = {
+            drag: true,
+            resize: true,
+            minimize: true,
+            maximize: true,
+            close: true,
+            onClose: false,
+            onMinimize: false,
+            onMaximize: false,
+            onDrag: false,
+            onResize: false
+        };
+        $.extend(control, options.control);
+
+        var attr = {
+            "data-drag": control.drag === true ? 1 : 0,
+            "data-top": css.top,
+            "data-left": css.left,
+            "data-width": css.width,
+            "data-height": css.height
+        };
+        if (control.minimize) attr['data-minimize'] = 0;
+        if (control.maximize) attr['data-maximize'] = 0;
+        $.extend(attr, options.attr);
+        attr.id = panel.id;
+
+        var body = {
+            html: '',
+            css: {
+                padding: 0,
+                height: parseInt(css.height) - 45
+            }
+        };
+        body = $.extend(true, {}, body, options.body);
+
+        /**
+         * TAB CREATE AND FUNCTIONS
+         * */
+
+        var $ontab = $("<div />").attr(attr).addClass("ontab").css({
+            "left": options.target ? parseInt(options.target.offset().left) + parseInt(options.target.width()*0.5) : wwt * 0.5,
+            "top": options.target ? parseInt(options.target.offset().top - $(window).scrollTop()) + parseInt(options.target.height()*0.5) : wht * 0.5
+        }).appendTo("body");
+        setTimeout(function () {
+            $ontab.css(css);
+        }, 1);
+
+        var $drag = $("<div />").addClass("ontab-header").prependTo($ontab);
+        $("<div />").addClass("title").text(panel.title).prependTo($drag);
+        $("<div />").addClass("ontab-content").css(body.css).html(body.html).prependTo($ontab);
+
+        if (control.minimize) {
+            var $mini = $("<div />").addClass("ontab-button btn-ontab-mini").attr("title", "minimizar").text("-").prependTo($drag);
+            $mini.on("click", function () {
+                if (control.onMinimize) {
+                    if (!control.onMinimize()) {
+                        panel.minimize($ontab);
+                    }
+                } else {
+                    panel.minimize($ontab);
+                }
+            });
+        }
+        if (control.maximize) {
+            var $maxi = $("<div />").addClass("ontab-button btn-ontab-maxi").attr("title", "maximizar").html("<div class='maxi'></div>").prependTo($drag);
+            $drag.on('dblclick', function () {
+                panel.maximize($ontab);
+            });
+            $maxi.on("click", function () {
+                if (control.onMaximize) {
+                    if (!control.onMaximize()) {
+                        panel.maximize($ontab);
+                    }
+                } else {
+                    panel.maximize($ontab);
+                }
+            });
+        }
+        if (control.close) {
+            var $close = $("<div />").addClass("ontab-button btn-ontab-close").attr("title", "fechar").text("x").prependTo($drag);
+            $close.on("click", function () {
+                if (control.onClose) {
+                    if (!control.onClose()) {
+                        panel.close($ontab);
+                    }
+                } else {
+                    panel.close($ontab);
+                }
+            });
+        }
+        if (control.drag) {
+            panel.drag($ontab, $drag);
+        }
+        if (control.resize) {
+            var $resize = $("<div />").addClass("ontab-resize").appendTo($ontab);
+            resize($ontab, $resize, control.resize, control.onResize, control.onDrag);
+        }
+
+        $ontab.css("z-index", getLastIndex($ontab)).on("mousedown", function () {
+            $(this).css("z-index", getLastIndex($(this)));
+        });
+
+        panelScrool(effect.blur);
+    };
+
+    $.fn.scrollBlock = function (enable) {
+        if (typeof(enable) === "undefined" || enable) {
+            window.oldScrollPos = $(window).scrollTop();
+
+            $(window).on('scroll.scrolldisabler', function (event) {
+                $(window).scrollTop(window.oldScrollPos);
+                event.preventDefault();
+            });
+        } else {
+            $(window).off('scroll.scrolldisabler');
+        }
+    };
+}(jQuery));
+
+
+var timeout;
+
+function blur() {
+    timeout = setTimeout( function() {
+        $("body").children("*:not(script, style, .ontab)").each(function () {
+            $(this).addClass('ontab-blur');
+        });
+    }, 75 );
+}
+
+function blurOut() {
+    clearTimeout( timeout );
+    $(".ontab-blur").removeClass('ontab-blur');
+}
+
+function closePanel($panel, blur) {
+    if ($panel.attr("data-minimize") === "1") {
+        $panel.attr("data-minimize", 0);
+        reazusteMinimalize();
+    }
+    $panel.remove();
+    panelScrool(blur);
+}
+
+function stop (event, M) {
+    $(document).off('mousemove mouseup');
+    return dragResizeModule(event, M);
+}
+
+function dragResizeAction ($ontab, $resize, key, haveResize, onResize, onDrag) {
+    $resize.on('mousedown', {e: $ontab, k: key}, function (v) {
+        $ontab.css({"transition-duration": "0s", "z-index": getLastIndex($ontab)});
+        var changeState = 0;
+        var mii = {'x': event.pageX, 'y': event.pageY};
+        var d = v.data, p = {};
+        var windowsTab = d.e;
+        if (windowsTab.css('position') !== 'relative') {
+            try {
+                windowsTab.position(p);
+            } catch (e) {
+            }
+        }
+        var M = {
+            h: $resize,
+            X: p.left || getInt(windowsTab, 'left') || 0,
+            Y: p.top || getInt(windowsTab, 'top') || 0,
+            W: getInt(windowsTab, 'width') || windowsTab[0].scrollWidth || 0,
+            H: getInt(windowsTab, 'height') || windowsTab[0].scrollHeight || 0,
+            pX: v.pageX,
+            pY: v.pageY,
+            k: d.k,
+            o: windowsTab
+        };
+
+        $(document).mousemove(function (event) {
+            if (changeState < 1) {
+                if (event.pageX > mii.x + 12 || event.pageX < mii.x - 12 || event.pageY > mii.y + 12 || event.pageY < mii.y - 12) {
+                    changeState = 1;
+                }
+            }
+
+            if ($ontab.attr("data-maximize") === "1" && changeState === 1) {
+                $ontab.attr("data-maximize", 0).css({
+                    'transition-duration': '0s',
+                    'width': parseInt($ontab.attr("data-width")) + 'px',
+                    'height': parseInt($ontab.attr("data-height")) + 'px'
+                }).find(".ontab-content").css("height", parseInt($ontab.attr("data-height")) - 45 + 'px');
+
+                if (haveResize) resize($ontab, $ontab.find(".ontab-resize").css("cursor", "se-resize"), haveResize, onResize, onDrag);
+            }
+
+            var newPosition = dragResizeModule(event, M);
+            windowsTab.css(newPosition);
+            if (M.k === "r") {
+                windowsTab.find(".ontab-content").css({height: newPosition.height - 45});
+            }
+
+        }).mouseup(function (event) {
+            if (changeState === 1) {
+                var newPosition = stop(event, M);
+                windowsTab.css(newPosition);
+                if (M.k === "r") {
+                    windowsTab.find(".ontab-content").css({height: newPosition.height - 45});
+                    if (onResize) onResize();
+                } else {
+                    if (onDrag) onDrag();
+                }
+                changeState = 0;
+            } else {
+                stop(event, M);
+                if ($ontab.attr("data-maximize") === "1") {
+                    $ontab.css({
+                        'top': -1 + 'px',
+                        'left': 0
+                    });
+                }
+            }
+        });
+
+        return false;
+    });
+}
+
+function resize ($ontab, $resize, resize, onResize, onDrag) {
+    dragResizeAction($ontab, $resize, 'r', resize, onResize, onDrag);
+}
+
+function minimizePanelOut($panel, speed, blur, resize, onResize, onDrag) {
+
+    $panel.find(".ontab-header").off("click");
+    $panel.attr("data-minimize", 0);
+
+    if ($panel.attr("data-maximize") === "1") {
+        maximizePanelIn($panel, resize);
+
+    } else {
+        $panel.css({
+            "top": parseInt($panel.attr("data-top")) + "px",
+            "left": parseInt($panel.attr("data-left")) + "px",
+            "width": parseInt($panel.attr("data-width")) + "px",
+            "height": parseInt($panel.attr("data-height")) + "px"
+        });
+        panelScrool(blur);
+    }
+
+    if ($panel.attr("data-drag") === "1") dragResizeAction($panel, $panel.find(".ontab-header"), 'd', resize, onResize, onDrag);
+
+    reazusteMinimalize();
+
+    setTimeout(function () {
+        $panel.css("transition-duration", "0s");
+    }, speed * 1000);
+}
+
+function minimizePanelIn($panel, speed, blur) {
+    if ($panel.attr("data-maximize") === "0") storePosition($panel);
+
+    var left = 0;
+    $(".ontab").each(function () {
+        if ($(this).attr("data-minimize") === "1") {
+            left += parseInt($(this).css("min-width"));
+        }
+    });
+
+    $panel.attr("data-minimize", 1).css({
+        "transition-duration": speed + "s",
+        "top": wht - 30 + "px",
+        "left": left,
+        "width": 0,
+        "height": 0
+    });
+
+    panelScrool(blur);
+}
+
+function maximizePanelOut($panel, speed, haveResize, onResize, onDrag) {
+    $panel.attr("data-maximize", 0).css({
+        "transition-duration": speed + "s",
+        'width': parseInt($panel.attr("data-width")) + 'px',
+        'height': parseInt($panel.attr("data-height")) + 'px',
+        'top': parseInt($panel.attr("data-top")) + 'px',
+        'left': parseInt($panel.attr("data-left")) + 'px'
+    }).find(".ontab-content").css("height", parseInt($panel.attr("data-height")) - 45 + 'px');
+
+    setTimeout(function () {
+        $panel.css("transition-duration", "0s");
+    }, speed * 1000);
+
+    if (haveResize) resize($panel, $panel.find(".ontab-resize").css("cursor", "se-resize"), haveResize, onResize, onDrag);
+}
+
+function maximizePanelIn($panel, speed, resize) {
+    if ($panel.attr("data-minimize") === "1") {
+        storePosition($panel);
+    }
+
+    $panel.attr("data-maximize", 1).css({
+        "transition-duration": speed + "s",
+        'width': '100%',
+        'height': '100%',
+        'top': '-1px',
+        'left': '0'
+    }).find(".ontab-content").css("height", wht - 45 + "px");
+
+    if(resize) $panel.find(".ontab-resize").off("mousedown").css("cursor", "initial");
+}
+
+function dragResizeModule (v, M) {
+    if (M.k === 'd') {
+        var left = M.X + v.pageX - M.pX;
+        var top = M.Y + v.pageY - M.pY;
+        left = left < 0 ? 0 : left;
+        top = top < -1 ? -1 : top;
+
+        if ((left !== 0 || top > -1) && M.o.attr("data-maximize") === '0') {
+            M.o.attr("data-left", left);
+            M.o.attr("data-top", top);
+        }
+
+        return {left: left, top: top};
+    } else {
+        var width = Math.max(v.pageX - M.pX + M.W, 0);
+        var height = Math.max(v.pageY - M.pY + M.H, 0);
+
+        M.o.attr("data-width", (wwt < parseInt(M.o.attr("data-left")) + width ? wwt - parseInt(M.o.attr("data-left")) : width));
+        M.o.attr("data-height", (wht < (parseInt(M.o.attr("data-top")) + height + 3) ? wht - parseInt(M.o.attr("data-top")) - 3 : height));
+
+        return {width: M.o.attr("data-width"), height: M.o.attr("data-height")};
+    }
+}
+
+function storePosition ($panel) {
+    $panel.attr({
+        "data-width": 2 + parseInt($panel.width() + parseInt($panel.css("padding-left")) + parseInt($panel.css("padding-right"))),
+        "data-height": 2 + parseInt($panel.height() + parseInt($panel.css("padding-top")) + parseInt($panel.css("padding-bottom"))),
+        "data-top": $panel.offset().top - $(window).scrollTop(),
+        "data-left": $panel.offset().left
+    });
+}
+
+function panelScrool (isBlur) {
+    var haveSomeOntabOpen = false;
+    $(".ontab").each(function () {
+        if ($(this).attr("data-minimize") === "0") {
+            haveSomeOntabOpen = true;
+        }
+    });
+
+    if (haveSomeOntabOpen) {
+        $("html").scrollBlock();
+        if(isBlur) blur();
+    } else {
+        $("html").scrollBlock(false);
+        if(isBlur) blurOut();
+    }
+}
+
+function getLastIndex ($tab) {
+    var zindex = parseInt($tab.css("z-index"));
+    $(".ontab").each(function () {
+        if ($(this).attr("data-minimize") === "0" && $(this).attr("id") !== $tab.attr("id")) {
+            zindex = (parseInt($(this).css("z-index")) >= zindex ? parseInt($(this).css("z-index")) + 1 : zindex);
+        }
+    });
+
+    return zindex;
+}
+
+function reazusteMinimalize() {
+    var i = 0;
+    $(".ontab").each(function () {
+        if ($(this).attr("data-minimize") === "1") {
+            $(this).css("left", i * parseInt($(this).css("min-width")));
+            i++;
+        }
+    });
+}
+
+function getInt (E, k) {
+    return parseInt(E.css(k)) || false;
+}
+
+$("#btnt").on("click", function () {
+    $(document).panel();
+});
