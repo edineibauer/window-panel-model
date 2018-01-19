3c80d8d14abf74a4698e27e6f1f2d676a564d521
Edinei <galera.org@gmail.com>
2018-01-19 11:49:59 -0200

    upd

--- panel.js
+++ panel.js
@@ -97,18 +97,16 @@ $(function ($) {
                     src: (typeof HOME !== 'undefined' ? HOME : "./") + "request/post"
                 },
                 css: {
-                    padding: 0,
-                    height: parseInt(css.height) - (control.resize ? 45 : 30)
+                    padding: 0
                 }
             };
             body = $.extend(true, {}, body, options.body);
-            body.css = processaCss(body.css);
 
             var $ontab = $("<div />").addClass("ontab").appendTo("body");
             var $header = $("<div />").addClass("ontab-header").addClass(header.class).prependTo($ontab);
             var $title = $("<div />").addClass("ontab-title").html(header.html).css(header.css).prependTo($header);
-            $header.css("background", $title.css("background")).css("height", $title.css("height"));
-            var $body = $("<div />").addClass("ontab-content").css(body.css).css("height", css.height - parseInt($header.height()) - 13).css("margin-top", (parseInt($title.css("height")) - 1)).html(body.html).prependTo($ontab);
+            $header.css("background", $title.css("background")).css("height", parseInt($title.height()));
+            var $body = $("<div />").addClass("ontab-content").css(body.css).css("margin-top", parseInt($header.css("height"))).html(body.html).prependTo($ontab);
 
             /* get WIDTH from auto css style */
             if (css.width === "auto") {
@@ -121,6 +119,7 @@ $(function ($) {
                 $ontab.css("height", "auto");
                 css.height = parseInt($ontab.height());
             }
+            css.height += control.resize ? 14 : 0;
 
             /* Converte RIGHT to LEFT position if Exist */
             if (typeof (css.right) !== "undefined") {
@@ -162,6 +161,10 @@ $(function ($) {
                             $body.panel(themeNotify("Erro ao carregar", "error"));
                     }
                 });
+            }
+
+            if(control.resize) {
+                $ontab.css("resize", "both");
             }
 
             if (control.minimize) {
@@ -672,4 +675,117 @@ function reazusteMinimalize() {
 
 function getInt(E, k) {
     return parseInt(E.css(k)) || false;
+}
+
+function themes(theme) {
+    if (theme.match(/erro/i))
+        return {background: '#f44336', color: "#FFFFFF"};
+    else if (theme.match(/(warn|alert|attem|aten|aviso)/i))
+        return {background: '#ff9800', color: "#FFFFFF"};
+    else
+        return {background: '#8bc34a', color: "#FFFFFF"};
+}
+
+function themesIcon(theme) {
+    if (theme.match(/(erro|dang)/i))
+        return "error";
+    else if (theme.match(/(warn|alert|attem|aten|aviso)/i))
+        return "warning";
+    else
+        return "done";
+}
+
+function themeWindow(title, param, funcao) {
+    return {
+        header: {
+            html: title
+        },
+        body: {
+            ajax: {
+                src: HOME + "request/post",
+                param: param,
+                done: funcao
+            },
+            css: {
+                padding: "0 15px"
+            }
+        }
+    };
+}
+
+function themeNotify(title, theme, time, position) {
+    /* check for theme and time set */
+    if (typeof (time) !== "undefined" && typeof (theme) === "number") {
+        var t = theme;
+        theme = time;
+        time = t;
+    } else if (typeof (theme) === "undefined") {
+        time = 2000;
+        theme = "infor";
+    } else if (typeof (time) === "undefined" && typeof (theme) === "number") {
+        time = theme;
+        theme = "infor";
+    }
+
+    var initial = {height: 50, width: 1000};
+    var cssPosition = {};
+    position = position || "right-top";
+    var action = "right";
+    $.each(position.match(/^\w+\s\w+$/i) ? position.split(' ') : position.split('-'), function (i, e) {
+        if (i === 0) {
+            action = e;
+            if (action === "bottom" || action === "top") initial = {height: 100, width: 200};
+        }
+        cssPosition[e] = "near";
+
+        if (action === "left" || action === "right")
+            initial[e] = (e === "bottom" || e === "top" ? "near" : -1000);
+        else
+            initial[e] = (e === "left" || e === "right" ? "near" : -100);
+    });
+
+    var css = themes(theme);
+    var themeIcon = themesIcon(theme);
+    css.padding = '15px';
+    css['font-size'] = "1.1em";
+    css['font-family'] = "roboto, sans-serif";
+    var retorno = {
+        initial: initial,
+        css: {
+            'width': 'auto',
+            'height': 'auto',
+            'border-radius': '5px',
+            'border-width': 0
+        },
+        header: {
+            css: {
+                "min-height": 0,
+                "height": 0,
+                "padding": 0
+            }
+        },
+        body: {
+            html: "<i class='material-icons'>" + themeIcon + "</i>" + title,
+            css: css
+        },
+        control: {
+            minimize: false,
+            maximize: false,
+            drag: false,
+            close: false,
+            clickOut: false,
+            resize: false,
+            timeOut: {
+                time: time,
+                out: action
+            }
+        }
+    };
+
+    if (typeof (cssPosition.right) === "undefined") retorno.css.left = cssPosition.left;
+    else retorno.css.right = cssPosition.right;
+    if (typeof (cssPosition.bottom) === "undefined") retorno.css.top = cssPosition.top;
+    else retorno.css.bottom = cssPosition.bottom;
+
+    return retorno;
 }
